f3908c6a9e6c6bb05987a4cb15f4f14fa9e1b191
Nathaniel Bibler <nate@envylabs.com>
2022-01-16 21:35:09 -0500

    fix: correctly hydrate the client store

--- shared/store.ts
+++ shared/store.ts
@@ -1,4 +1,9 @@
-import { configureStore, createSlice, ThunkAction } from "@reduxjs/toolkit";
+import {
+  configureStore,
+  createSlice,
+  PayloadAction,
+  ThunkAction,
+} from "@reduxjs/toolkit";
 import { Action } from "redux";
 import { createWrapper, HYDRATE } from "next-redux-wrapper";
 
@@ -13,17 +18,19 @@ interface Features {
 export const featureFlagsSlice = createSlice({
   extraReducers: {
     [HYDRATE]: (state, action) => {
-      console.log("HYDRATE", state, action.payload);
       return {
+        ...action.payload.featureFlags,
         ...state,
-        ...action.payload.subject,
       };
     },
   },
-  initialState: { COLOR: "blue" } as Features,
+  initialState: {} as Features,
   name: "featureFlags",
   reducers: {
-    setFeatureFlag(state, action) {
+    setFeatureFlag<F extends Feature>(
+      state: Features,
+      action: PayloadAction<{ name: F; value: Features[F] }>
+    ) {
       return {
         ...state,
         [action.payload.name]: action.payload.value,
